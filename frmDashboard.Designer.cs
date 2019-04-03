e49f1665f1c0db0b01e5e9663c9874a32b690e5a
phamsonghuong <1554050046huong@ou.edu.vn>
2019-04-04 03:34:34 +0700

    Thiet ke form Dashboard

---
+++ frmDashboard.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace QuanLyMyPham
+{
+    partial class frmDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnNguoiDung = new System.Windows.Forms.Button();
+            this.btnNCC = new System.Windows.Forms.Button();
+            this.btnTonKho = new System.Windows.Forms.Button();
+            this.btnHoaDon = new System.Windows.Forms.Button();
+            this.btnSanPham = new System.Windows.Forms.Button();
+            this.btnDangXuat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(618, 29);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(119, 37);
+            this.btnThoat.TabIndex = 12;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            // 
+            // btnNguoiDung
+            // 
+            this.btnNguoiDung.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNguoiDung.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNguoiDung.Location = new System.Drawing.Point(322, 302);
+            this.btnNguoiDung.Name = "btnNguoiDung";
+            this.btnNguoiDung.Size = new System.Drawing.Size(119, 37);
+            this.btnNguoiDung.TabIndex = 6;
+            this.btnNguoiDung.Text = "Người Dùng";
+            this.btnNguoiDung.UseVisualStyleBackColor = false;
+            // 
+            // btnNCC
+            // 
+            this.btnNCC.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNCC.Location = new System.Drawing.Point(322, 233);
+            this.btnNCC.Name = "btnNCC";
+            this.btnNCC.Size = new System.Drawing.Size(119, 37);
+            this.btnNCC.TabIndex = 7;
+            this.btnNCC.Text = "Nhà Cung Cấp";
+            this.btnNCC.UseVisualStyleBackColor = false;
+            // 
+            // btnTonKho
+            // 
+            this.btnTonKho.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTonKho.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTonKho.Location = new System.Drawing.Point(519, 233);
+            this.btnTonKho.Name = "btnTonKho";
+            this.btnTonKho.Size = new System.Drawing.Size(119, 37);
+            this.btnTonKho.TabIndex = 8;
+            this.btnTonKho.Text = "Tồn Kho";
+            this.btnTonKho.UseVisualStyleBackColor = false;
+            // 
+            // btnHoaDon
+            // 
+            this.btnHoaDon.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHoaDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHoaDon.Location = new System.Drawing.Point(112, 302);
+            this.btnHoaDon.Name = "btnHoaDon";
+            this.btnHoaDon.Size = new System.Drawing.Size(119, 37);
+            this.btnHoaDon.TabIndex = 9;
+            this.btnHoaDon.Text = "Hóa Đơn";
+            this.btnHoaDon.UseVisualStyleBackColor = false;
+            // 
+            // btnSanPham
+            // 
+            this.btnSanPham.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSanPham.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSanPham.Location = new System.Drawing.Point(112, 233);
+            this.btnSanPham.Name = "btnSanPham";
+            this.btnSanPham.Size = new System.Drawing.Size(119, 37);
+            this.btnSanPham.TabIndex = 10;
+            this.btnSanPham.Text = "Sản Phẩm";
+            this.btnSanPham.UseVisualStyleBackColor = false;
+            // 
+            // btnDangXuat
+            // 
+            this.btnDangXuat.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDangXuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangXuat.Location = new System.Drawing.Point(519, 302);
+            this.btnDangXuat.Name = "btnDangXuat";
+            this.btnDangXuat.Size = new System.Drawing.Size(119, 37);
+            this.btnDangXuat.TabIndex = 11;
+            this.btnDangXuat.Text = "Đăng xuất";
+            this.btnDangXuat.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(143, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(457, 51);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Beauty Shop Xin Chào";
+            // 
+            // frmDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(767, 439);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnNguoiDung);
+            this.Controls.Add(this.btnNCC);
+            this.Controls.Add(this.btnTonKho);
+            this.Controls.Add(this.btnHoaDon);
+            this.Controls.Add(this.btnSanPham);
+            this.Controls.Add(this.btnDangXuat);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDashboard";
+            this.Text = "Quản lý bán mỹ phẩm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnNguoiDung;
+        private System.Windows.Forms.Button btnNCC;
+        private System.Windows.Forms.Button btnTonKho;
+        private System.Windows.Forms.Button btnHoaDon;
+        private System.Windows.Forms.Button btnSanPham;
+        private System.Windows.Forms.Button btnDangXuat;
+        private System.Windows.Forms.Label label1;
+    }
+}
