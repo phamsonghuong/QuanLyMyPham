fb1be0cc9436540a100a603409460443f047ec8c
vithihoaichang <1554050010chang@ou.edu.vn>
2019-04-15 15:16:22 +0700

    Xy ly unit test Them NCC

--- TestSupplier.cs
+++ TestSupplier.cs
@@ -11,7 +11,6 @@ namespace QuanLyMyPham
 {
     public class TestSupplier
     {
-        
         public static string ktThemNCC(string name, string address, int phone)
         {
             string connectionString = @"Data Source=DESKTOP-BV4LABJ\SQLEXPRESS;Initial Catalog=QLMyPham;Integrated Security=True";
@@ -40,34 +39,8 @@ namespace QuanLyMyPham
 
             }
         }
-        public static string ktSuaNCC(string name, string address, int phone)
-        {
-            string connectionString = @"Data Source=DESKTOP-BV4LABJ\SQLEXPRESS;Initial Catalog=QLMyPham;Integrated Security=True";
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                string ten = name;
-                string dc = address;
-                int sdt = phone;
-
-                string Query = "Insert into tbl_supplier values(@name,@address,@phone)";
-                SqlCommand cmd = new SqlCommand(Query, conn);
-                cmd.Parameters.AddWithValue("@name", ten);
-                cmd.Parameters.AddWithValue("@address", dc);
-                cmd.Parameters.AddWithValue("@phone", sdt);
-                conn.Open();
-                int a = cmd.ExecuteNonQuery();
-                if (a > 0)
-                {
-                    return "1";
-                }
-                else
-                {
-                    return "0";
-                }
-                conn.Close();
-
-            }
-        }
+       
+            
 
     }
 
