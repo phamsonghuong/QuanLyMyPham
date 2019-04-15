bcbedbe9abec8fbff8e20fa9cdad5572ab8972be
vithihoaichang <1554050010chang@ou.edu.vn>
2019-04-15 14:30:16 +0700

    Xu ly unit test - them NCC

--- TestSupplier.cs
+++ TestSupplier.cs
@@ -3,10 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuanLyMyPham;
+using System.Data.SqlClient;
+
 
 namespace QuanLyMyPham
 {
-    class TestSupplier
+    public class TestSupplier
     {
+        
+        public static string ktThemNCC(string name, string address, int phone)
+        {
+            string connectionString = @"Data Source=DESKTOP-BV4LABJ\SQLEXPRESS;Initial Catalog=QLMyPham;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string ten = name;
+                string dc = address;
+                int sdt = phone;
+
+                string Query = "Insert into tbl_supplier values(@name,@address,@phone)";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+                cmd.Parameters.AddWithValue("@name",ten);
+                cmd.Parameters.AddWithValue("@address", dc);
+                cmd.Parameters.AddWithValue("@phone", sdt);
+                conn.Open();
+                int a = cmd.ExecuteNonQuery();
+                if (a > 0)
+                {
+                    return "1";
+                }
+                else
+                {
+                    return "0";
+                }
+                conn.Close();
+
+            }
+        }
+        public static string ktSuaNCC(string name, string address, int phone)
+        {
+            string connectionString = @"Data Source=DESKTOP-BV4LABJ\SQLEXPRESS;Initial Catalog=QLMyPham;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string ten = name;
+                string dc = address;
+                int sdt = phone;
+
+                string Query = "Insert into tbl_supplier values(@name,@address,@phone)";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+                cmd.Parameters.AddWithValue("@name", ten);
+                cmd.Parameters.AddWithValue("@address", dc);
+                cmd.Parameters.AddWithValue("@phone", sdt);
+                conn.Open();
+                int a = cmd.ExecuteNonQuery();
+                if (a > 0)
+                {
+                    return "1";
+                }
+                else
+                {
+                    return "0";
+                }
+                conn.Close();
+
+            }
+        }
+
     }
-}
+
+ }
