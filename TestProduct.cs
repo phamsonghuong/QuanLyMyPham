0dd378be40b594a89c6d5d110fda106658b4ae9b
vithihoaichang <1554050010chang@ou.edu.vn>
2019-04-15 15:37:15 +0700

    Xu ly unit test Them San pham

--- TestProduct.cs
+++ TestProduct.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace QuanLyMyPham
 {
-    class TestProduct
+    public class TestProduct
     {
+        public static string ktSanpham(string name, double price, int quantity, string unit, string suppliername)
+        {
+            string connectionString = @"Data Source=DESKTOP-BV4LABJ\SQLEXPRESS;Initial Catalog=QLMyPham;Integrated Security=True";
 
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string ten = name;
+                string donvi = unit;
+                string tenncc = suppliername;
+                double gia = price;
+                int soluong = quantity;
+
+                string Query = "Insert into tbl_product values(@name,@price,@quantity,@unit,@suppliername)";
+                SqlCommand cmd = new SqlCommand(Query, conn);
+                cmd.Parameters.AddWithValue("@name", ten);
+                cmd.Parameters.AddWithValue("@price", gia);
+                cmd.Parameters.AddWithValue("@quantity", soluong);
+                cmd.Parameters.AddWithValue("@unit", donvi);
+                cmd.Parameters.AddWithValue("@suppliername", tenncc);
+
+                conn.Open();
+
+                int a = cmd.ExecuteNonQuery();
+                if (a > 0)
+                {
+                    return "1";
+                }
+                else
+                {
+                    return "0";
+                }
+                conn.Close();
+
+            }
+        }
     }
 }
